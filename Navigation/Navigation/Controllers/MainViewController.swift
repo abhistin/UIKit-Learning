56e9ef7499b8dcf608013a9fa4193a3d3cdda081
Abhishek Bhardwaj <=>
2024-01-07 22:31:48 +0530

    Basic Navigation with TableViews, fetching Live weather data

---
+++ Navigation/Navigation/Controllers/MainViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by Abhishek Bhardwaj on 05/01/24.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var mainVCLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mainVCLabel.text = "Welcome To Simple Navigation App."
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func navigateToFirstVC(_ sender: UIButton) {
+        let firstVC = self.storyboard?.instantiateViewController(withIdentifier: "FirstViewController") as! FirstViewController
+        firstVC.firstVCString = "This String is being set from Main VC."
+        self.navigationController?.pushViewController(firstVC, animated: true)
+    }
+    
+    @IBAction func navigateToSecondVC(_ sender: UIButton) {
+        let secondVC = self.storyboard?.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+        secondVC.secondVCString = "This String is being set from Main VC."
+        self.navigationController?.pushViewController(secondVC, animated: true)
+    }
+    
+}
+
