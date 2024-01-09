8767f4237cdfa3ac9677125b84c7f2e6b4fe219e
Abhishek Bhardwaj <=>
2024-01-09 10:33:26 +0530

    Initial Commit

---
+++ Navigation/BasicUserDefaults/BasicUserDefaults/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  BasicUserDefaults
+//
+//  Created by Abhishek Bhardwaj on 09/01/24.
+//
+
+import UIKit
+
+struct Colors {
+    static let darkGrey  = UIColor(red: 40/255, green: 40/255, blue: 40/255, alpha: 1)
+    static let lightBlue = UIColor(red: 89/255, green: 205/255, blue: 242/255, alpha: 1)
+}
---
+++ Navigation/BasicUserDefaults/BasicUserDefaults/NameTextField.swift
@@ -0,0 +1,41 @@
+//
+//  NameTextField.swift
+//  BasicUserDefaults
+//
+//  Created by Abhishek Bhardwaj on 09/01/24.
+//
+
+import UIKit
+
+class NameTextField: UITextField {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpTextField()
+    }
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init( coder: aDecoder )
+        setUpTextField()
+    }
+    
+    
+    private func setUpTextField() {
+        tintColor       = Colors.darkGrey
+        textColor       = Colors.darkGrey
+        backgroundColor = .white
+    }
+    
+    
+    func updateStyle(isDarkMode: Bool) {
+        let mainColor       = isDarkMode ? .white : Colors.darkGrey
+        keyboardAppearance  = isDarkMode ? .dark : .light
+        
+        UIView.animate(withDuration: 0.4) {
+            self.tintColor          = mainColor
+            self.textColor          = mainColor
+            self.backgroundColor    = isDarkMode ? Colors.darkGrey : .white
+        }
+    }
+}
